00105666b4419ffe98998504b3eca17bab892798
Diogo Biazus <diogo.biazus@coinberry.com>
2020-01-03 17:34:33 -0500

    Add circle configuration to run rippled during tests

--- .circleci/setupenv.sh
+++ .circleci/setupenv.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y hlint
+apt -y update
+apt -y install hlint apt-transport-https ca-certificates wget gnupg
+
+# Setup mock rippled server
+wget -q -O - "https://repos.ripple.com/repos/api/gpg/key/public" | apt-key add -
+echo "deb https://repos.ripple.com/repos/rippled-deb bionic stable" | sudo tee -a /etc/apt/sources.list.d/ripple.list
+apt -y update
+apt -y install rippled
+
 
 # Setup mock parity server
 curl -O https://releases.parity.io/ethereum/v2.5.9/x86_64-unknown-linux-gnu/parity
