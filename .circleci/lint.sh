c9bce20b63008736a13a62e7a744fbc0d3c03924
Diogo Biazus <diogo.biazus@coinberry.com>
2020-02-27 23:29:22 -0500

    We don't need to lint tests

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git ls-files . | grep -e '.*\.hs' | grep -v -e "test-pure" -e "test-impure" | xargs hlint "$@"
+git ls-files . | grep -e '.*\.hs' | grep -v -e "test" | xargs hlint "$@"
